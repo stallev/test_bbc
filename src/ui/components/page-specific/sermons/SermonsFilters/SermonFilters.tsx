184c878dfeac8b97216f6d4c174a40f73115e7bc
Alexander <stwork87@gmail.com>
2024-01-25 15:02:18 +0300

    added date picker

--- src/ui/components/page-specific/sermons/SermonsFilters/SermonFilters.tsx
+++ src/ui/components/page-specific/sermons/SermonsFilters/SermonFilters.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
+import DatePicker from 'react-datepicker';
 import Container from '@/ui/containers/Container/Container';
 
 import styles from './styles/sermons-filters.module.scss';
+import "react-datepicker/dist/react-datepicker.css";
 
 import { SermonsFiltersProps } from './types';
 
 const SermonFilters: React.FC<SermonsFiltersProps> = ({ data }) => {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+
   console.log(data)
   const booksOptions = data.biblebooks ? data.biblebooks.map((book: string) => {
     return { value: book, label: book };
@@ -23,24 +27,31 @@ const SermonFilters: React.FC<SermonsFiltersProps> = ({ data }) => {
   return (
     <Container>
       <div className={styles["sermons-filters"]}>
-        <Select
-          id="long-value-select"
-          instanceId="long-value-select"
-          options={booksOptions}
-          className={styles["sermons-filters__item"]}
-         />
-        <Select
-          id="long-value-select2"
-          instanceId="long-value-select2"
-          options={preachersOptions}
-          className={styles["sermons-filters__item"]}
-         />
-        <Select
-          id="long-value-select3"
-          instanceId="long-value-select3"
-          options={topicsOptions}
-          className={styles["sermons-filters__item"]}
-         />
+        <div className={styles["sermons-filters__categories"]}>
+          <Select
+            id="long-value-select"
+            instanceId="long-value-select"
+            options={booksOptions}
+            className={styles["sermons-filters__item"]}
+          />
+          <Select
+            id="long-value-select2"
+            instanceId="long-value-select2"
+            options={preachersOptions}
+            className={styles["sermons-filters__item"]}
+          />
+          <Select
+            id="long-value-select3"
+            instanceId="long-value-select3"
+            options={topicsOptions}
+            className={styles["sermons-filters__item"]}
+          />
+        </div>
+
+         <div className={styles["sermons-filters__dates"]}>
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+         </div>
       </div>
     </Container>
   )
