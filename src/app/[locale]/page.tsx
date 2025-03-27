3074b798c7db7624bd7375cd407094f90b639e31
Alexander <stwork87@gmail.com>
2025-03-27 12:16:44 +0300

    add full app

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,43 +1,43 @@
-// import dynamic from "next/dynamic";
-// import { Metadata } from 'next'
+import dynamic from "next/dynamic";
+import { Metadata } from 'next'
 import { getTranslations } from "@/utils/languageParser";
 import { PagesIDs, RoutePath } from "@/constants";
-// import UpcomingEventsDataApi from "@/services/UpcomingDataApi";
-// import YouTubeApiService from "@/services/YouTubeApi";
-// import StaffDataApi from "@/services/StaffDataApi";
-// import BlogDataApi from "@/services/BlogDataApi";
-// import { YouTubePlaylistIDs, YouTubeApiKeys } from "@/constants";
-// import { MAP_IDs } from "@/constants/mock";
-// import PageContentDataApi from "@/services/PageDataApi";
+import UpcomingEventsDataApi from "@/services/UpcomingDataApi";
+import YouTubeApiService from "@/services/YouTubeApi";
+import StaffDataApi from "@/services/StaffDataApi";
+import BlogDataApi from "@/services/BlogDataApi";
+import { YouTubePlaylistIDs, YouTubeApiKeys } from "@/constants";
+import { MAP_IDs } from "@/constants/mock";
+import PageContentDataApi from "@/services/PageDataApi";
 import { i18n, Locale } from "@/i18n.config";
 import GreetingScreen from "@/ui/components/page-specific/home/GreetingScreen/GreetingScreen";
 import FixedPageLink from "@/ui/components/FixedPageLink/FixedPageLink";
-// import Staff from "@/ui/components/page-specific/home/Staff/Staff";
-// import Container from "@/ui/containers/Container/Container";
+import Staff from "@/ui/components/page-specific/home/Staff/Staff";
+import Container from "@/ui/containers/Container/Container";
 import { getPagePathData } from "@/utils/getPostSeoData";
 import { getSeoData } from "@/utils/getSeoData";
 import { PagePathProps } from "@/types/globalTypes";
 
-// const UpcomingEvents = dynamic(() => import('@/ui/components/page-specific/home/UpcomingEvents/UpcomingEvents'));
-// const Ministries = dynamic(() => import('@/ui/components/page-specific/home/Ministries/Ministries'));
-// const LiveStreamsDynamic = dynamic(() => import('@/ui/components/page-specific/home/LiveStreams/LiveStreams'));
-// const PastorsBlog = dynamic(() => import('@/ui/components/page-specific/home/PastorsBlog/PastorsBlog'));
-// const Donation = dynamic(() => import('@/ui/components/Donation/Donation'));
-// const MapLocation = dynamic(() => import('@/ui/components/MapLocation/MapLocation'));
+const UpcomingEvents = dynamic(() => import('@/ui/components/page-specific/home/UpcomingEvents/UpcomingEvents'));
+const Ministries = dynamic(() => import('@/ui/components/page-specific/home/Ministries/Ministries'));
+const LiveStreamsDynamic = dynamic(() => import('@/ui/components/page-specific/home/LiveStreams/LiveStreams'));
+const PastorsBlog = dynamic(() => import('@/ui/components/page-specific/home/PastorsBlog/PastorsBlog'));
+const Donation = dynamic(() => import('@/ui/components/Donation/Donation'));
+const MapLocation = dynamic(() => import('@/ui/components/MapLocation/MapLocation'));
 
-// export async function generateMetadata(
-//   { params: { locale } }: PagePathProps
-// ): Promise<Metadata> {
-//   const pageId = locale == i18n.defaultLocale ? PagesIDs.Home[i18n.defaultLocale] : PagesIDs.Home.ru;
+export async function generateMetadata(
+  { params: { locale } }: PagePathProps
+): Promise<Metadata> {
+  const pageId = locale == i18n.defaultLocale ? PagesIDs.Home[i18n.defaultLocale] : PagesIDs.Home.ru;
 
-//   const { seo: seoContentData } = await PageContentDataApi.getPageContentData(pageId);
-//   const seoPathData = getPagePathData({
-//     locale,
-//     path: RoutePath.Home
-//   });
+  const { seo: seoContentData } = await PageContentDataApi.getPageContentData(pageId);
+  const seoPathData = getPagePathData({
+    locale,
+    path: RoutePath.Home
+  });
 
-//   return getSeoData({seoContentData, seoPathData});
-// }
+  return getSeoData({seoContentData, seoPathData});
+}
 
 export default async function Home({
   params: { locale }
@@ -46,13 +46,13 @@ export default async function Home({
 }) {
   const translations = getTranslations(locale);
   
-  // const upcomingEventsData = await UpcomingEventsDataApi.getUpcomingEventsReduced(locale);
-  // const videosData = await YouTubeApiService.getPortionYouTubeStreamsItems(
-  //   YouTubePlaylistIDs.generalLiveStreams,
-  //   YouTubeApiKeys.alexander
-  // );
-  // const staffData = await StaffDataApi.getMinisters(locale);
-  // const postsData = await BlogDataApi.getLastPostsDataHomePageByLang(locale);
+  const upcomingEventsData = await UpcomingEventsDataApi.getUpcomingEventsReduced(locale);
+  const videosData = await YouTubeApiService.getPortionYouTubeStreamsItems(
+    YouTubePlaylistIDs.generalLiveStreams,
+    YouTubeApiKeys.alexander
+  );
+  const staffData = await StaffDataApi.getMinisters(locale);
+  const postsData = await BlogDataApi.getLastPostsDataHomePageByLang(locale);
 
   return (
     <>
@@ -68,7 +68,7 @@ export default async function Home({
         about_church_link_label={translations.about_church_nav_link_text}
       />
 
-      {/* <LiveStreamsDynamic
+      <LiveStreamsDynamic
         data={videosData}
         locale={locale}
       />
@@ -85,9 +85,9 @@ export default async function Home({
         <Donation isDonationPage={false} translations={translations} />
       </Container>
 
-      <MapLocation mapId={MAP_IDs.homePage} /> */}
+      <MapLocation mapId={MAP_IDs.homePage} />
     </>
   );
 }
 
-// export const revalidate = 5 * 60;
+export const revalidate = 5 * 60;
