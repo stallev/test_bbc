9c131376ec88e93f54b02e32995802cc8df84f3c
Alexander <stwork87@gmail.com>
2025-04-01 13:19:03 +0300

    changed caching settings5

--- next.config.js
+++ next.config.js
@@ -68,15 +68,15 @@ const nextConfig = {
           },
         ],
       },
-      {
-        source: '/_next/static/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
+      // {
+      //   source: '/_next/static/:path*',
+      //   headers: [
+      //     {
+      //       key: 'Cache-Control',
+      //       value: 'public, max-age=31536000, immutable',
+      //     },
+      //   ],
+      // },
       // {
       //   source: '/:locale(en|ru)/:path*',
       //   headers: [
