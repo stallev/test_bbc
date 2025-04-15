0ba65bd934ef6609b2bfeeba2bd99484e818a139
Alexander <stwork87@gmail.com>
2025-04-15 21:14:22 +0300

    try to correct React minified error

--- src/ui/components/page-specific/live-streams/YearStreamsList/YearStreamsList.tsx
+++ src/ui/components/page-specific/live-streams/YearStreamsList/YearStreamsList.tsx
@@ -57,7 +57,7 @@ const YearStreamsList = ({
 
   const orderedMonths = isDesktopSize(width)
     ? [0, 4, 8, 1, 5, 9, 2, 6, 10, 3, 7, 11]
-    : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
   const selectedVideos = data.monthListArray.find(
     month => month.monthNumber === selectedStreamsPeriod.month
