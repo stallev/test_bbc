b33e44ee611f0db75bec97c20387326b6b524cb0
Alexander <stwork87@gmail.com>
2025-04-15 23:28:31 +0300

    try to correct React minified error 12

--- src/ui/components/page-specific/live-streams/YearStreamsList/YearStreamsList.tsx
+++ src/ui/components/page-specific/live-streams/YearStreamsList/YearStreamsList.tsx
@@ -57,7 +57,7 @@ const YearStreamsList = ({
 
   const orderedMonths = isDesktopSize(width)
     ? [0, 4, 8, 1, 5, 9, 2, 6, 10, 3, 7, 11]
-    : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
   const selectedVideos = data.monthListArray.find(
     month => month.monthNumber === selectedStreamsPeriod.month
@@ -103,7 +103,7 @@ const YearStreamsList = ({
         </div>
       </div>
 
-      <div
+      {/* <div
         className={`${styles['year-streams-list__content']} ${
           isThisYearSelected ? styles['year-streams-list__content--expanded'] : ''
         }`}
@@ -114,7 +114,7 @@ const YearStreamsList = ({
             <YouTubePlayer key={video.id} data={video} locale={locale} />
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
