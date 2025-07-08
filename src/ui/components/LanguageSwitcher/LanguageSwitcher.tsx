78c5c7b3896139360ce57c99c9a7d98e63ae7427
Alexander <stwork87@gmail.com>
2025-07-08 18:25:51 +0300

    F-126: added newPathname console output 6

--- src/ui/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/ui/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -11,7 +11,6 @@ import { getPathnameParams } from '@/utils/languageParser';
 import styles from './styles/language-switcher.module.scss';
 
 const LanguageSwitcher = () => {
-  // const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const pathname = usePathname();
 
@@ -25,8 +24,6 @@ const LanguageSwitcher = () => {
   const handleLanguageChange = () => {
     setIsLoading(true);
     document.cookie = `${LANGUAGE_COOKIE_NAME}=${availableLocale}; max-age=${LANGUAGE_COOKIE_MAX_AGE}; path=/`;
-    console.log('newPathname', newPathname);
-    // router.push(newPathname);
   };
 
   useEffect(() => {
