2037f36463d7e9659e8e42f52b23c0acba56d9c1
Alexander <stwork87@gmail.com>
2025-08-02 14:12:12 +0300

    F-132: added loading.tsx 6

--- src/app/[locale]/ministries/[postSlug]/page.tsx
+++ src/app/[locale]/ministries/[postSlug]/page.tsx
@@ -1,10 +1,10 @@
-import { Metadata } from 'next';
-import { RoutePath } from '@/constants/RoutePath';
-import MinistryDataApi from '@/services/MinistryDataApi';
+// import { Metadata } from 'next';
+// import { RoutePath } from '@/constants/RoutePath';
+// import MinistryDataApi from '@/services/MinistryDataApi';
 import { PostParams } from '@/types/postTypes';
 import MinistryPageWrapper from '@/ui/components/page-specific/ministry/MinistryPageWrapper/MinistryPageWrapper';
-import { getPagePathData } from '@/utils/getPostSeoData';
-import { getSeoData } from '@/utils/getSeoData';
+// import { getPagePathData } from '@/utils/getPostSeoData';
+// import { getSeoData } from '@/utils/getSeoData';
 
 export async function generateStaticParams() {
   return [];
@@ -12,17 +12,17 @@ export async function generateStaticParams() {
 
 export const revalidate = 60;
 
-export async function generateMetadata(props: { params: Promise<PostParams> }): Promise<Metadata> {
-  const params = await props.params;
-  const { locale, postSlug } = params;
-  const ministryPageData = await MinistryDataApi.getMinistryPageData(postSlug, locale);
-  const seoContentData = ministryPageData?.seo;
-  const seoPathData = getPagePathData({
-    locale,
-    path: `${RoutePath.Ministries}/${postSlug}`,
-  });
-  return getSeoData({ seoContentData, seoPathData });
-}
+// export async function generateMetadata(props: { params: Promise<PostParams> }): Promise<Metadata> {
+//   const params = await props.params;
+//   const { locale, postSlug } = params;
+//   const ministryPageData = await MinistryDataApi.getMinistryPageData(postSlug, locale);
+//   const seoContentData = ministryPageData?.seo;
+//   const seoPathData = getPagePathData({
+//     locale,
+//     path: `${RoutePath.Ministries}/${postSlug}`,
+//   });
+//   return getSeoData({ seoContentData, seoPathData });
+// }
 
 export default async function PostMinistry(props: { params: Promise<PostParams> }) {
   const { postSlug, locale } = await props.params;
