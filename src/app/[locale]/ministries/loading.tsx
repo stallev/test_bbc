a95460ba8ca75565851442fd3f8be5641ccfe32d
Alexander <stwork87@gmail.com>
2025-08-03 04:06:09 +0300

    F-132: added loading.tsx 13

--- src/app/[locale]/ministries/loading.tsx
+++ src/app/[locale]/ministries/loading.tsx
@@ -1,10 +1,37 @@
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import styles from '@/styles/pages/ministry.module.scss';
+import Container from '@/ui/containers/Container/Container';
+
 export default function LoadingMinistryPage() {
   return (
-    <div>
-      <div style={{ height: '40px', width: '200px', background: '#ccc', marginBottom: '1rem' }} />
-      <div style={{ height: '20px', width: '100%', background: '#eee', marginBottom: '0.5rem' }} />
-      <div style={{ height: '20px', width: '100%', background: '#eee', marginBottom: '0.5rem' }} />
-      <div style={{ height: '300px', width: '100%', background: '#ddd' }} />
-    </div>
+    <SkeletonTheme baseColor="#ccc" highlightColor="#ddd">
+      <Container>
+        <div className={styles['ministry__layout-skeleton']}>
+          <h1 className={styles['ministry__title']}>
+            <Skeleton className={styles['ministry__layout-title-skeleton']} width={250} />
+          </h1>
+
+          <div className={styles['ministry__layout-gallery']}>
+            <div className={styles['ministry__layout-gallery-item']}>
+              <Skeleton height="100%" />
+            </div>
+            <div className={styles['ministry__layout-gallery-item']}>
+              <Skeleton height="100%" />
+            </div>
+            <div className={styles['ministry__layout-gallery-item']}>
+              <Skeleton height="100%" />
+            </div>
+            <div className={styles['ministry__layout-gallery-item']}>
+              <Skeleton height="100%" />
+            </div>
+          </div>
+
+          <div className={styles['ministry__layout-content-skeleton']}>
+            <Skeleton count={10} height={16} style={{ marginBottom: '12px' }} />
+          </div>
+        </div>
+      </Container>
+    </SkeletonTheme>
   );
 }
